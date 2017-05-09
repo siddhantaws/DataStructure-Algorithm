01e178725f6d10fd39649435908cfeed1ab13a27
siddhantaws <siddhantaws@gmail.com>
2017-05-09 23:49:11 +0530

    maven

---
+++ Maven.bat
@@ -0,0 +1,5 @@
+set JAVA_HOME=C:/Program Files/Java\jdk1.8.0_91
+set MAVNE_HOME=D:/Program Files/Apache Software Foundation/apache-maven-3.1.1
+set path=%JAVA_HOME%/bin;%MAVNE_HOME%/bin;path%;
+cd bom
+mvn clean install -Dmaven.test.skip=true
