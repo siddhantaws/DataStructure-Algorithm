4d42b55c82eba8b233e076d1249a54c36c23f250
siddhantaws <siddhantaws@gmail.com>
2017-05-24 23:27:55 +0530

    Calculate Power of a number using devide and conqur

---
+++ algorithm/divide-conquer/src/main/java/com/wfs/devideConqur/MedianOfTwoSortedArrays.java
@@ -0,0 +1,10 @@
+package com.wfs.devideConqur;
+
+/**
+ * Created by Suryasnata on 5/24/2017.
+ * Question: There are 2 sorted arrays A and B of size n each.
+ * Write an algorithm to find the median of the array obtained after merging the above 2 arrays(i.e. array of length 2n). The complexity should be O(log(n))
+ */
+public class MedianOfTwoSortedArrays {
+
+}
