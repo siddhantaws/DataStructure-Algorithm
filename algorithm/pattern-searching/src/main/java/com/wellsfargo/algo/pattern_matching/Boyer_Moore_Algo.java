7728fbc836bb017b29291b85c7b2b51283214c69
siddhantaws <siddhantaws@gmail.com>
2017-09-26 10:05:35 +0530

    Bad Match table

--- algorithm/pattern-searching/src/main/java/com/wellsfargo/algo/pattern_matching/Boyer_Moore_Algo.java
+++ algorithm/pattern-searching/src/main/java/com/wellsfargo/algo/pattern_matching/Boyer_Moore_Algo.java
@@ -1,5 +1,7 @@
 package com.wellsfargo.algo.pattern_matching;
 
+import java.util.Map;
+
 public class Boyer_Moore_Algo  extends PatternMatch {
 
     private int badCharacterArray[][] ;
@@ -8,8 +10,9 @@ public class Boyer_Moore_Algo  extends PatternMatch {
 
     public Boyer_Moore_Algo(String text, String pattern) {
         super(text, pattern);
-        this.badCharacterArray =new int[patternlength][numberOfUniqueCharacter];
+        this.badCharacterArray =new int[numberOfUniqueCharacter][patternlength];
         this.goodSuffixArray =new int[patternlength];
+        init();
     }
 
     @Override
@@ -33,11 +36,36 @@ public class Boyer_Moore_Algo  extends PatternMatch {
 
     private void constructBadMatchTable()
     {
-
+        for(int i=0;i<numberOfUniqueCharacter;i++)
+            for(int j=0;j<patternlength;j++)
+                badCharacterArray[i][j] = Integer.MAX_VALUE;
+        constructBadMatchTable(patternlength-1);
     }
+
+    private void constructBadMatchTable(int index)
+    {
+       while (index>=0)
+       {
+           Character ch =pattern.charAt(index);
+           int k=0 , charIndex =characterToIndexMap.get(ch);
+           badCharacterArray[charIndex][index]=k;
+           for(int i=index+1;( i<patternlength && badCharacterArray[charIndex][i]==Integer.MAX_VALUE);i++)
+               badCharacterArray[charIndex][i]=++k;
+           constructBadMatchTable(index-1);
+           for(int i=index-1; i>=0 && badCharacterArray[charIndex][i]==Integer.MAX_VALUE;i--)
+               badCharacterArray[charIndex][i]=i+1;
+           index --;
+       }
+    }
+
 
     private void constructGoodSuffixTable()
     {
+
+    }
+
+    public static void main(String[] args) {
+        PatternMatch patternMatch=new Boyer_Moore_Algo("AGTCTDSHDHDHD","ATGCTAGC");
 
     }
 }
